a9a266eb164b967aae513247c34ae52b26e193e2
t0ha <t0hashvein@gmail.com>
2014-05-07 12:21:01 +0600

    It works

--- frontend.py
+++ frontend.py
@@ -1,22 +1,38 @@
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-#!/usr/bin/env python
 
-import gtk
-import webkit
+from gi.repository import Gtk as gtk, Gdk as gdk, GLib
+from gi.repository.WebKit import WebView
 
-gtk.gdk.threads_init()
+GLib.threads_init()
 
-class browser():
+
+class browser(WebView):
     def __init__(self, uri):
-        view = webkit.WebView()
-        viewMF = view.get_main_frame()
+        WebView.__init__(self)
+        viewMF = self.get_main_frame()
         sw = gtk.ScrolledWindow()
-        sw.add(view)
-        win = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        win.connect('destroy', quit)
-        win.set_default_size(600, 400)
-        win.add(sw)
-        win.show_all()
+        sw.add(self)
+        self.win = gtk.Window()
+        self.win.connect('destroy', quit)
+        self.connect('download-requested', self._download_destination_cb)
+        self.win.set_default_size(600, 400)
+        self.win.add(sw)
+        self.win.show_all()
         viewMF.load_uri(uri)
+
+    def _download_destination_cb(self, view, download):
+        """docstring for _download_destination_cb"""
+        fc = gtk.FileChooserDialog(title="Save file as...", parent=None, buttons=(gtk.STOCK_SAVE, gtk.ResponseType.OK,gtk.STOCK_CANCEL, gtk.ResponseType.CANCEL), action=gtk.FileChooserAction.SAVE)
+        fc.set_default_response(gtk.ResponseType.OK)
+        fname = download.get_suggested_filename()
+        fc.set_current_name(fname)
+        res = fc.run()
+        if  res == gtk.ResponseType.OK:
+            uri = fc.get_uri()
+            download.set_destination_uri(uri)
+        fc.destroy()
+        return True
+
 browser('http://localhost:8000')
 gtk.main()
