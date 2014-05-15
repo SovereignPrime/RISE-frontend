6d112d386816da9e943e8da007ec84bda8634107
t0ha <t0hashvein@gmail.com>
2014-05-15 12:23:04 +0600

    added backend start

--- frontend.py
+++ frontend.py
@@ -15,7 +15,7 @@ class browser(WebView):
         sw = gtk.ScrolledWindow()
         sw.add(self)
         self.win = gtk.Window(title="RISE")
-        self.win.connect('destroy', quit)
+        self.win.connect('destroy', self.quit)
         self.connect('download-requested', self._download_destination_cb)
         self.win.set_default_size(600, 400)
         self.win.add(sw)
@@ -24,20 +24,33 @@ class browser(WebView):
 
     def _download_destination_cb(self, view, download):
         """docstring for _download_destination_cb"""
-        fc = gtk.FileChooserDialog(title="Save file as...", parent=None, buttons=(gtk.STOCK_SAVE, gtk.ResponseType.OK,gtk.STOCK_CANCEL, gtk.ResponseType.CANCEL), action=gtk.FileChooserAction.SAVE)
+        fc = gtk.FileChooserDialog(title="Save file as...",
+                                   parent=None,
+                                   buttons=(gtk.STOCK_SAVE,
+                                            gtk.ResponseType.OK,
+                                            gtk.STOCK_CANCEL,
+                                            gtk.ResponseType.CANCEL),
+                                   action=gtk.FileChooserAction.SAVE)
         fc.set_default_response(gtk.ResponseType.OK)
         fname = download.get_suggested_filename()
         fc.set_current_name(fname)
         res = fc.run()
-        if  res == gtk.ResponseType.OK:
+        if res == gtk.ResponseType.OK:
             uri = fc.get_uri()
             download.set_destination_uri(uri)
         fc.destroy()
         return True
 
+    def quit(self):
+        """docstring for quit"""
+        gtk.main_quit()
+        os.system('./rise_service stop')
+
 if __name__ == '__main__':
+    os.system('./rise_service start')
     while 'RISE_HTTP_PORT' in os.environ:
         time.sleep(1)
     port = os.environ['RISE_HTTP_PORT']
+    print port
     browser('http://localhost:%s' % port)
     gtk.main()
