60d2619528d78bb544d62c89e1045ff42f3caf3e
t0ha <t0hashvein@gmail.com>
2014-05-19 13:45:27 +0600

    Some dll added

--- setup.py
+++ setup.py
@@ -11,16 +11,21 @@ from glob import glob
 data_files = [("Microsoft.VC90.CRT", glob(r'C:\Program Files\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.CRT\*.*'))]
 
 missing_dll = ['libgtk-3-0.dll',
-                              'libgdk-3-0.dll',
-                              'libatk-1.0-0.dll',
-                              'libcairo-gobject-2.dll',
-                              'libgdk_pixbuf-2.0-0.dll',
-                              'libjpeg-8.dll',
-                              'libpango-1.0-0.dll',
-                              'libpangocairo-1.0-0.dll',
-                              'libpangoft2-1.0-0.dll',
-                              'libpangowin32-1.0-0.dll',
-                              'libgnutls-26.dll'
+               'libgdk-3-0.dll',
+               'libatk-1.0-0.dll',
+               'libcairo-gobject-2.dll',
+               'libgdk_pixbuf-2.0-0.dll',
+               'libjpeg-8.dll',
+               'libpango-1.0-0.dll',
+               'libpangocairo-1.0-0.dll',
+               'libpangoft2-1.0-0.dll',
+               'libpangowin32-1.0-0.dll',
+               'libgnutls-26.dll',
+               'libglib-2.0-0.dll',
+               'libwebkitgtk-3.0-0.dll',
+               'libgirepository-1.0-1.dll',
+               'libglib-2.0-0.dll',
+               'libffi-6.dll'
                ]
 
 setup(name='RISE',
