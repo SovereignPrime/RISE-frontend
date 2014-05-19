ba048f0d0afc77c8b66abf87910467930fd40152
t0ha <t0hashvein@gmail.com>
2014-05-19 09:13:04 +0600

    setup as in example

--- setup.py
+++ setup.py
@@ -3,12 +3,22 @@
 # vin: ts=4 sw=4 et ft=python
 
 from distutils.core import setup
-import os
+import os, sys
 import py2exe
 
 from glob import glob
 
 data_files = [("Microsoft.VC90.CRT", glob(r'C:\Program Files\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.CRT\*.*'))]
 
-setup(console=['frontend.py'])
+setup(name='RISE',
+      description='Secure sharing and collaboration',
+      version='0.0.30',
 
+      console=['frontend.py'],
+
+      options={
+          'py2exe': {
+              'packages': ['encodings'],
+              'includes': ['gobject', 'webkit']
+          }
+      })
